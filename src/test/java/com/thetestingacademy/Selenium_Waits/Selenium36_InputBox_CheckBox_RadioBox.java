b60ee13466040afd192cb5e2ac3d0adeef0b99a5
vivek <rathorevivek050@gmail.com>
2025-01-04 12:59:50 +0530

    selenium

---
+++ src/test/java/com/thetestingacademy/Selenium_Waits/Selenium36_InputBox_CheckBox_RadioBox.java
@@ -0,0 +1,22 @@
+package com.thetestingacademy.Selenium_Waits;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Selenium36_InputBox_CheckBox_RadioBox {
+
+    public static void main(String[] args) {
+
+        EdgeDriver driver = new EdgeDriver();
+        driver.get("https://awesomeqa.com/practice.html");
+
+        driver.findElement(By.name("firstname")).sendKeys("the testing academy");
+
+        // RADIO Box
+        driver.findElement(By.id("sex-1")).click();
+
+        // CheckBox
+        driver.findElement(By.id("tool-1")).click();
+
+    }
+}
