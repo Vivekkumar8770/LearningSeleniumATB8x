95050f89704ce30e63d3d4c78ebf5de052798340
vivek <rathorevivek050@gmail.com>
2025-01-05 15:00:58 +0530

    selenium

--- src/test/java/com/thetestingacademy/SVG_Shadow_DOM/Selenium44_WebTables_Static.java
+++ src/test/java/com/thetestingacademy/SVG_Shadow_DOM/Selenium44_WebTables_Static.java
@@ -64,7 +64,7 @@ public class Selenium44_WebTables_Static {
             for (int j = 1; j <= col; j++) {
                 String dynamic_xpath = first_part + i + second_part + j + third_part;
                 String data = driver.findElement(By.xpath(dynamic_xpath)).getText();
-//                System.out.println(data);
+                System.out.println(data);
                 if (data.contains("Helen Bennett")) {
 
                     String country_path = dynamic_xpath + "/following-sibling::td";
